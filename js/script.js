06d85d25b655a728644de3ac9bf733ca70955a10
RaidenBorunda <137729767+RaidenBorunda@users.noreply.github.com>
2023-06-25 15:34:34 -0700

    Assignment 1

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+// 3.14 = numberic literal expression
+// "I like Pie!" = string literal expression
+// true = Boolean literal expression
+// null = literal 
+
+// STEP 3
+// const x = 1 + 2 * 3;
+// let a = 10 ; let b = 20; alert(a == 3 || b == 10);
+
+// STEP 4
+// let firstName
+// let lastName
+// let address
+// let city
+// let state
+// let lZipCode
+// let lYourAge
+// let referralSource
+// let mayWeContactYou
+
+// STEP 5
+
+// var firstName = "Raiden";
+// let lZipCode = 92027;
+// const city = "Escondido";
+
+// STEP 6
+var firstCarNumber = 1997;
+var firstCarModel = "Civic";
+console.log(firstCarNumber + firstCarModel)
+
+
+// STEP 7
+// STEP 8
+// STEP 9
+// STEP 10
+// STEP 11
+// STEP 12
+// STEP 13
+// STEP 14
+// STEP 15
+// STEP 16
+// STEP 17
+// STEP 18
+// STEP 19
+// STEP 20
+// STEP 21
+// STEP 22
