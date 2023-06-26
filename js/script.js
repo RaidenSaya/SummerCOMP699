9f2d0b3df904305c717b2d4d2d7fea175d0feb56
RaidenBorunda <137729767+RaidenBorunda@users.noreply.github.com>
2023-06-25 17:09:07 -0700

    Assignment 1

--- js/script.js
+++ js/script.js
@@ -16,41 +16,91 @@
 // let a = 10 ; let b = 20; alert(a == 3 || b == 10);
 
 // STEP 4
-// let firstName
-// let lastName
-// let address
-// let city
-// let state
-// let lZipCode
-// let lYourAge
-// let referralSource
-// let mayWeContactYou
+// let firstName;
+// let strLastName;
+// let address;
+// let city;
+// let state;
+// let lZipCode;
+// let lYourAge;
+// let referralSource;
+// let mayWeContactYou;
 
 // STEP 5
-
 // var firstName = "Raiden";
 // let lZipCode = 92027;
 // const city = "Escondido";
 
 // STEP 6
-var firstCarNumber = 1997;
-var firstCarModel = "Civic";
-console.log(firstCarNumber + firstCarModel)
-
+// var firstCar = 1997 + " Honda Civic";
+// console.log(firstCar);
 
 // STEP 7
+// var favoriteColor = myBoolean + color; console.log(favoriteColor);
+// var boilingWater = myBoolean + 212; console.log(boilingWater);
+
 // STEP 8
+// I would make the following updates to get the string:
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+// window.console.log(someString);
+
 // STEP 9
+// var hungry = null; console.log(hungry);
+// var thirsty = undefined; console.log(thirsty);
+
 // STEP 10
+// console.log(typeof 7);
+// console.log(typeof true);
+// console.log(typeof "Hello!");
+// console.log(typeof Object());
+// console.log(typeof x);
+
 // STEP 11
+// alert("Hello " + "Raiden Borunda" + ", welcome to the JavaScript class!");
+
 // STEP 12
+// var name = "Raiden Borunda";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
 // STEP 13
+// var course = "JavaScript";
+// alert("Hello " + "Raiden Borunda" + ", welcome to the " + course + " class!");
+
 // STEP 14
+// var course = "JavaScript"; var name = prompt("Please enter your name.");
+// alert("Hello " + "name" + "\nWelcome to the " + course + " class!");
+
 // STEP 15
+// var course = "JavaScript"; var name = prompt("Please enter your name.");
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
 // STEP 16
+// var name = prompt("Please enter your name."); var course = prompt("What class are you taking?");
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
 // STEP 17
+// var x = 10;
+// var y = 20;
+// console.log(x+y);
+
 // STEP 18
+// var x = 20;
+// console.log(x = x += 20);
+
 // STEP 19
+// var x = 20;
+// console.log(x = x *= 5);
+
 // STEP 20
+// var x = (20 % 3);
+// console.log(x /= 1);
+
 // STEP 21
+// var x = 4;
+// var y = 8;
+// alert(x >= 4 && y == 8);
+
 // STEP 22
+// var x = 10;
+// var y = 20;
+// alert(x <= 5 || y != 20);
