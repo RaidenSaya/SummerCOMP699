1539de03e72dd38e4968d35357a2897769620f9d
RaidenBorunda <137729767+RaidenBorunda@users.noreply.github.com>
2023-06-26 19:17:08 -0700

    Assignment 2

--- Assignment 2/js/03-coinflip.js
+++ Assignment 2/js/03-coinflip.js
@@ -1,18 +1,14 @@
 let randomNum = Math.round(Math.random());
 let coinFlip = randomNum;
-
-
 var heads;
-
 
 if (coinFlip == 0){
     heads = true
 } else {
     heads = false
 }
-console.log(coinFlip,heads);
 let choice = prompt("Heads or Tails?");
-console.log(choice.toUpperCase());
+
 if (choice.toUpperCase() == "HEADS" && heads == true){
     alert("The flip was heads and you chose heads... you win!");
 } else if (choice.toUpperCase() == "HEADS" && heads == false){
--- Assignment 2/js/04-grade-assigner.js
+++ Assignment 2/js/04-grade-assigner.js
@@ -1,14 +1,15 @@
 var userNumber = prompt("Please pick a number between 1-100");
+userNumber = parseInt(userNumber);
 var validNumber = false;
 while (validNumber == false) {
-    if (parseInt(userNumber) > 0 && parseInt(userNumber) <= 100){
+    if (userNumber > 0 && userNumber <= 100){
         validNumber = true;
     } else {
         validNumber = false
         userNumber = prompt("That number is not valid. Please pick a number between 1-100");
     }
 }
-userNumber = parseInt(userNumber);
+
 if (userNumber < 60) {
     console.log("You received an F");
 } else if (userNumber >= 60 && userNumber <= 69){
