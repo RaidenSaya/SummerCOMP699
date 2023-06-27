c935592675494c0a69906cf83d3e3c4c123103d4
RaidenBorunda <137729767+RaidenBorunda@users.noreply.github.com>
2023-06-26 19:49:12 -0700

    01-larger-or-smaller_update

--- Assignment 2/js/01-larger-or-smaller.js
+++ Assignment 2/js/01-larger-or-smaller.js
@@ -2,6 +2,8 @@ let num1 = prompt("Please enter a number.");
 let num2 = prompt("Please enter another number.");
 if (Number(num1) > Number(num2)) {
     document.write(num1);
+} else if (num1 == num2) {
+    document.write("Both of these numbers are the same: "+ num1)
 } else {
     document.write(num2);
 }
