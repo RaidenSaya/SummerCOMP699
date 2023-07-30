4f5437bfbef89d24a8c79ecef8fa7d5c92869be3
RaidenBorunda <137729767+RaidenBorunda@users.noreply.github.com>
2023-07-30 16:40:19 -0700

    update 1a

--- Assignment 6/js/main.js
+++ Assignment 6/js/main.js
@@ -2,11 +2,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
     let btn = window.document.getElementById("btnAddEmployee");
-    // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
     btn.addEventListener("click", function () {     
         // SET WIDTH AND HEIGHT VARIABLES TO 800 x 700
         let width = 800
         let height = 700
+        // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
         myWindow = window.open('add-employee.html', 'popup', 'resizeable=yes')
         // RESIZE THE POPUP TO THE SET WIDTH AND HEIGHT
         myWindow.resizeTo(width,height)
